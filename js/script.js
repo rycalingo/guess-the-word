9545129cebd1cf5c679258b5a05a8848c2f8cc93
Ryan Calingo <rycalingo@gmail.com>
2024-03-13 15:41:00 -0400

    Updating variable value

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const guessButton = document.querySelector(".guess");
 const letterInput = document.querySelector("#letter");
 const progressDisplay = document.querySelector(".word-in-progress");
 const remainingDisplay = document.querySelector(".remaining");
-const remainingGuesses = `<span></span> guesses`;
+const remainingGuessesSpan = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
