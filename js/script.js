dac7bcc5efc03f7208f176b8930562bd80c53e90
Ryan Calingo <rycalingo@gmail.com>
2024-03-13 00:54:07 -0400

    Adding global variables

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const gussedDisplay = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const letterInput = document.querySelector("#letter");
+const progressDisplay = document.querySelector(".word-in-progress");
+const remainingDisplay = document.querySelector(".remaining");
+const remainingGuesses = `<span></span> guesses`;
+const message = document.querySelector(".message");
+const playAgainButton = document.querySelector(".play-again");
+
+const word = "magnolia";
